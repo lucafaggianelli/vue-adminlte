0ece707b06b8c1d43cec65c846a324abe05f300b
Luca <luca.faggianelli@gmail.com>
2017-04-17 22:40:24 +0200

    Add BoxDocs

--- docs/DocsLayout.vue
+++ docs/DocsLayout.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <h1>Hello docs</h1>
 
+    <box-docs></box-docs>
     <box-small-docs></box-small-docs>
     <info-box-docs></info-box-docs>
     <progress-bar-docs></progress-bar-docs>
@@ -10,6 +11,7 @@
 </template>
 
 <script>
+import BoxDocs from './examples/BoxDocs.vue'
 import BoxSmallDocs from './examples/BoxSmallDocs.vue'
 import InfoBoxDocs from './examples/InfoBoxDocs.vue'
 import ProgressBarDocs from './examples/ProgressBarDocs.vue'
@@ -17,6 +19,7 @@ import UserWidgetDocs from './examples/UserWidgetDocs.vue'
 
 export default {
   components: {
+    BoxDocs,
     BoxSmallDocs,
     InfoBoxDocs,
     ProgressBarDocs,
---
+++ docs/examples/BoxDocs.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="page-header">
+      <h1>Box</h1>
+    </div>
+
+    <example-section :sourcecode="source">
+      <box
+          title="Box Title"
+          tagline="Pictures"
+          icon="info">
+        <h2>Content here</h2>
+        <ol>
+          <li>Hello</li>
+        </ol>
+      </box>
+    </example-section>
+
+    <props-section :props="props"></props-section>
+
+  </div>
+</template>
+
+<script>
+import { Box } from '../../src/widgets'
+
+export default {
+  data: function () {
+    return {
+      props: Box.props,
+      source: `<box-small
+    header="3000"
+    tagline="Pictures"
+    icon="camera"
+    color="bg-green"></box-small>`
+    }
+  }
+}
+</script>
