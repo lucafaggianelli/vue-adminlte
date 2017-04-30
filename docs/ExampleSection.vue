e7d200d29d253f19adfddc13d08c9720a6f8a14b
Luca <luca.faggianelli@gmail.com>
2017-04-30 14:41:44 +0200

    [docs] Example styling
    highlighjs style atom-one-dark

--- docs/ExampleSection.vue
+++ docs/ExampleSection.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="row">
 
-    <div class="col-sm-offset-3 col-sm-6 col-xs-12">
-      <slot></slot>
+    <div class="example-wrapper">
+      <div class="col-sm-offset-3 col-sm-6 col-xs-12">
+        <slot></slot>
+      </div>
     </div>
 
-    <div class="col-xs-12">
-      <pre v-highlightjs="sourcecode">
-        <code class="html"></code>
-      </pre>
+    <div class="col-xs-12 source-wrapper">
+      <pre v-highlightjs="sourcecode"><code class="html"></code></pre>
     </div>
 
   </div>
@@ -22,3 +22,25 @@ export default {
   }
 }
 </script>
+<style>
+.example-wrapper {
+  border: 1px solid #f0f0f0;
+  background-color: #f8f8f8;
+  margin: 0 15px;
+  padding-top: 1em;
+}
+.example-wrapper:after {
+  content:'';
+  display:block;
+  clear: both;
+}
+
+.source-wrapper pre {
+  border: none;
+  border-radius: 0;
+  padding: 0;
+}
+.source-wrapper pre code {
+  padding: 2em 3em;
+}
+</style>
